e26e8c348dd0ee6a5e4147746254492a6c0d2863
Aisling Kinch <N00255605@iadt.ie>
2026-04-13 12:48:41 +0100

    13

--- src/project/books/index.php
+++ src/project/books/index.php
@@ -4,6 +4,8 @@ require_once 'php/lib/utils.php';
 
 try {
     $books = Book::findAll();
+    $publishers = Publisher::findAll();
+    $formats = Format::findAll();
 
 } 
 catch (PDOException $e) {
@@ -14,10 +16,6 @@ catch (PDOException $e) {
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <link rel="stylesheet" href="/project/books/css/all.min.css">
-        <link rel="stylesheet" href="/project/books/css/grid.css">
-        <link rel="stylesheet" href="/project/books/css/reset.css">
-        <link rel="stylesheet" href="/project/books/css/style.css">
         <?php include 'php/inc/head_content.php'; ?>
         <title>Books</title>
     </head>
@@ -31,6 +29,40 @@ catch (PDOException $e) {
                 <div class="button">
                     <a href="book_create.php">Add New Book</a>
                 </div>
+
+                <?php if (!empty($books)) { ?>
+                <div class="width-12 filters">
+                    <form>
+                        <div>
+                            <label for="title_filter">Title:</label>
+                            <input type="text" id="title_filter" name="title_filter">
+                        </div>
+                        <div>
+                            <label for="publisher_filter">Publisher:</label>
+                            <select id="publisher_filter" name="publisher_filter">
+                                <option value="">All Publisher</option>
+                                <?php foreach ($publishers as $publisher) { ?>
+                                    <option value="<?= h($publisher->id) ?>"><?= h($publisher->name) ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <label for="format_filter">Format:</label>
+                            <select id="format_filter" name="format_filter">
+                                <option value="">All Format</option>
+                                <?php foreach ($formats as $format) { ?>
+                                    <option value="<?= h($format->id) ?>"><?= h($format->name) ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+                        <div>
+                            <button type="button" id="apply_filters">Apply Filters</button>
+                            <button type="button" id="clear_filters">Clear Filters</button>
+                        </div>
+                    </form>
+                </div>
+            <?php } ?>
+        </div>
             </div>
 
         </div>
