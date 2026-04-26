3cf3f4ea3e1b6a1558be851a355d1037bfa07acc
Aisling <N00255605@iadt.ie>
2026-04-26 17:51:12 +0100

    testing 25/04

--- src/project/books/index.php
+++ src/project/books/index.php
@@ -2,6 +2,36 @@
 require_once 'php/lib/config.php';
 require_once 'php/lib/utils.php';
 
+// Cookie session for dark mode
+if (isset($_GET['cookie_theme'])){
+    $theme = $_GET['cookie_theme'];
+
+    // Store in cookie for 30 days
+    setcookie('theme', $theme, time() + (60 * 60 * 24 * 30), '/');
+
+    // Redirect index
+    header('Location: index.php');
+    exit;
+}
+
+// reset
+if (isset($_GET['reset_cookie'])){
+    $no = time();
+    $expiry = time() - 3600;
+    setcookie('theme', '', $expiry, '/');
+
+    header('Location: index.php');
+    exit;
+}
+
+// Theme options
+$cookieTheme = isset($_COOKIE['theme']) ? $_COOKIE['theme'] : 'not set';
+
+$themes = [
+    'light' => ['bg' => '#ffffff', 'text' => '#333333'],
+    'dark' => ['bg' => '#7070e2', 'text' => '#eaeaea'],
+];
+
 try {
     $books = Book::findAll();
     $publishers = Publisher::findAll();
@@ -19,65 +49,59 @@ catch (PDOException $e) {
         <?php include 'php/inc/head_content.php'; ?>
         <title>Books</title>
     </head>
-    <body>
+
+    <body class="<?= $cookieTheme ? $cookieTheme : '' ?>">
+    <div class="header">
         <div class="container">
             <div class="width-12">
                 <?php require 'php/inc/flash_message.php'; ?>
             </div>
-            <div class="width-12 header">
+        
+
+        <div class="width-12 header_contents">
                 <div class="button">
                     <a href="book_create.php">Add New Book</a>
                 </div>
-            </div>
-
-            <?php if (!empty($books)) { ?>
-                <div class="width-12">
-                    <form id="filters" class="filters">
-                        <div class="input">
-                            <label class="filter-label" for="title_filter">Title:</label>
+                        
+                <?php if (!empty($books)) { ?>
+                    <div class="width-12 filters">
+                        <form>
                             <div>
-                                <input type="text" id="title_filter" name="title_filter" placeholder="Part of a title">
+                                <label for="title_filter">Title:</label>
+                                <input type="text" id="title_filter" name="title_filter">
                             </div>
-                        </div>
 
-                        <!-- ~~~~~~~~~Publishers Filter~~~~~~~~~ -->
-                        <div class="input">
-                            <label class="filter-label" for="publisher_filter">Publisher:</label>
                             <div>
+                                <label for="publisher_filter">Publisher:</label>
                                 <select id="publisher_filter" name="publisher_filter">
                                     <option value="">All Publisher</option>
-                                    <?php foreach ($publishers as $publisher): ?>
-                                        <option value="<?= htmlspecialchars($publisher->id) ?>">
-                                            <?= htmlspecialchars($publisher->name) ?>
-                                        </option>
-                                    <?php endforeach; ?>
+                                    <?php foreach ($publishers as $publisher) { ?>
+                                        <option value="<?= h($publisher->id) ?>"><?= h($publisher->name) ?></option>
+                                    <?php } ?>
                                 </select>
                             </div>
-                        </div>
 
-                        <!-- ~~~~~~~~~Format Filter~~~~~~~~~ -->
-                        <div class="input">
-                            <label class="filter-label" for="format_filter">Format:</label>
                             <div>
+                                <label for="format_filter">Format:</label>
                                 <select id="format_filter" name="format_filter">
-                                    <option value="">All Formats</option>
-                                    <?php foreach ($formats as $format): ?>
-                                        <option value="<?= htmlspecialchars($format->id) ?>">
-                                            <?= htmlspecialchars($format->name) ?>
-                                        </option>
-                                    <?php endforeach; ?>
+                                    <option value="">All Format</option>
+                                    <?php foreach ($formats as $format) { ?>
+                                        <option value="<?= h($format->id) ?>"><?= h($format->name) ?></option>
+                                    <?php } ?>
                                 </select>
                             </div>
-                        </div>
 
-                        <div>
-                            <button type="button" id="apply_filters">Apply Filters</button>
-                            <button type="button" id="clear_filters">Clear Filters</button>
-                        </div>
-                    </form>
-                </div>
-            <?php } ?>
+                            <div class="clearApplyButtons">
+                                <button type="button" id="apply_filters">Apply Filters</button>
+                                <button type="button" id="clear_filters">Clear Filters</button>
+                            </div>
+                            
+                        </form>
+                    </div>
+                <?php } ?>
         </div>
+        </div>
+    </div>
 
         <!-- books -->
         <div class="container">
@@ -85,21 +109,8 @@ catch (PDOException $e) {
                 <p>No books found.</p>
             <?php } else { ?>
                 <div class="width-12 cards">
-                    <?php 
-                    foreach ($books as $book) { 
-                        $bookFormats = Format::findByBook($book->id);
-                        $formatIds = [];
-                        foreach ($bookFormats as $f) {
-                            $formatIds[] = $f->id;
-                        }
-                        $formatIdsStr = implode(' ', $formatIds);
-                    ?>
-                        <div class="card"
-                             data-title="<?= $book->title ?>"
-                             data-publisher="<?= $book->publisher_id ?>"
-                             data-formats="<?= $formatIdsStr ?>"
-                             data-year="<?= $book->year ?>"
-                        >
+                    <?php foreach ($books as $book) { ?>
+                        <div class="card">
                             <div class="top-content">
                                 <img src="images/<?= ($book->cover_filename) ?>" />
                                 <h2>Title: <?= ($book->title) ?></h2>
@@ -120,6 +131,25 @@ catch (PDOException $e) {
             <?php } ?>
         </div>
 
-        <script src="js/filters.js"></script>
+    <div class="footer">
+        <div class="container">
+            <div class="width-12 footer_contents">
+                
+                <div class="button">
+                    <a href="?cookie_theme=light">Light</a> 
+                </div>
+
+                <div class="button">
+                    <a href="?cookie_theme=dark">Dark</a> 
+                </div>
+
+                <div class="button">
+                    <a href="?reset_cookie=1">Reset</a>
+                </div> 
+
+            </div>
+        </div>
+    </div>
+        <script src="filters.js"></script>
     </body>
 </html>
